73bdebf44df4b8b824df3a06ea8627956ac2c89c
Anthony <anthony.fabregat@gmail.com>
2017-12-08 16:13:29 +0100

    'message'

--- connexion.php
+++ connexion.php
@@ -62,22 +62,6 @@ if(!empty($_POST['submitted'])) {
 include_once('./inc/header.php');
 ?>
   <form method="POST" action="connexion.php" id="formconnexion">
-<<<<<<< HEAD
-    <div class="form-group">
-			<label for="pseudo">Pseudo ou mail<span>*</span></label>
-      <p class="error"><?php if(!empty($errors['pseudo'])) { echo $errors['pseudo']; } ?></p>
-
-			<?php nouvelInputSQL($textLabel='pseudo',$typeInput='text',$nomInput='pseudo',$placeholder='pseudo',$errors); ?>
-    </div>
-
-		<?php nouvelInputSQL($textLabel='Entrez votre mot de passe',$typeInput='password',$nomInput='password',$placeholder='Votre mot de passe',$errors) ?>
-    <label>
-      <input type="checkbox" name="remember" /> Se souvenir de moi
-    </label>
-    <br/>
-    <?php inputSubmit() ?>
-	  <a href="passwordforget.php">Mot de passe perdu</a>
-=======
 		<?php nouvelInputSQL($textLabel='pseudo',$typeInput='text',$nomInput='pseudo',$placeholder='pseudo',$errors);
 		nouvelInputSQL($textLabel='Entrez votre mot de passe',$typeInput='password',$nomInput='password',$placeholder='Votre mot de passe',$errors); ?>
 		<div class="checkbox">
@@ -92,7 +76,6 @@ include_once('./inc/header.php');
 			<div class="label"></div>
 			<a href="./forgotten-password.php"><p>Mot de passe oublié ?</p></a>
 		</div>
->>>>>>> 7fa562daa65eadfdb4da955d691924f56471357e
   </form>
 
 
