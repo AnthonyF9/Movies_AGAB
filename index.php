5b1d7611d7fb66886c5b95ff4c16fa55ce707cc4
Anthony <anthony.fabregat@gmail.com>
2017-12-08 11:42:57 +0100

    'message'

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$title = 'Accueil';
+include_once('./inc/pdo.php');
+include_once('./inc/fonctions.php');
+session_start();
+include_once('./inc/header.php');
+?>
+
+
+      <main>
+
+      </main>
+
+
+<?php
+include_once('./inc/footer.php');
+?>
