ce47f3a201dc29eebcbc6ece0dbe5b4eae6b2c73
Anthony <anthony.fabregat@gmail.com>
2017-12-08 11:50:22 +0100

    'message'

--- index.php
+++ index.php
@@ -5,19 +5,14 @@ include_once('./inc/fonctions.php');
 session_start();
 include_once('./inc/header.php');
 ?>
-<<<<<<< HEAD
-
 
       <main>
 
-      </main>
 
 
-=======
-      <main>
-        
+
       </main>
->>>>>>> 986364bd4d19470e8a22a0bad6a5f65bf7dc03ce
+
 <?php
 include_once('./inc/footer.php');
 ?>
