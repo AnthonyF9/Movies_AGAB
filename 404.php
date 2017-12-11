baaa6d73e1935d56316935e9ca701320bb0f7678
Guillaume Giudici <gui.giudici@gmail.com>
2017-12-11 09:36:18 +0100

    gui

---
+++ 404.php
@@ -0,0 +1,36 @@
+<?php
+$title = 'Accueil';
+// include_once('./inc/pdo.php');
+// include_once('./inc/fonctions.php');
+// session_start();
+include_once('./cookies.php');
+include_once('./inc/header.php');
+?>
+      <main>
+        <div class="flex-col">
+          <div id="alpha">
+            <?php
+            
+            ?>
+          </div>
+          <div id="beta">
+            <?php
+
+            ?>
+            <img src="http://graphiste.kezaco.fr/wp-content/uploads/2014/01/Erreur404-graphiste-lyon.jpg" alt="erreur 404" />
+          </div>
+          <div id="gamma">
+            <?php
+
+            ?>
+          </div>
+          <div id="delta">
+            <?php
+
+            ?>
+          </div>
+        </div>
+      </main>
+<?php
+include_once('./inc/footer.php');
+?>
