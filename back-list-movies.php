fb75f2309bda29a0a1552573c6cf372b1b674595
beryl <beryl.de-vos@orange.fr>
2017-12-12 09:39:37 +0100

    mess1

--- back-list-movies.php
+++ back-list-movies.php
@@ -19,29 +19,34 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $movies = $stmt->fetchAll();
 ?>
+<main>
+  <div id="wrapper">
+    <div id="alpha">
+      <table>
+        <tr>
+          <th>id</th>
+          <th>title</th>
+          <th>year</th>
+          <th>rating</th>
+          <th>actions</th>
+        </tr>
+        <?php
+        foreach ($movies as $movie) {
 
-<a href="details.php"></a>
+           echo '<tr><td>'. $movie['id'] .'</td>';
+           echo '<td>'. $movie['title'] . '</td>';
+           echo '<td>'. $movie['year'] . '</td>';
+           echo '<td>'. $movie['rating'] . '</td>';
+           echo '<td><a href="details.php?movie='. $movie['slug'].'" class="btn btn-success">Voir sur le site</a>
+                      <a href="back-modif.php?id='. $movie['id'].'" class="btn btn-success">Modifier</a>
+                      <a href="back-delete.php?id='. $movie['id'].'" class="btn btn-success">Effacer</a>
+                      </td></tr>';
+         }
+         ?>
+      </table>
+    </div>
+  </div>
+</main>
 
-  <table class="table">
-    <tr>
-      <th>id</th>
-      <th>title</th>
-      <th>year</th>
-      <th>rating</th>
-      <th>actions</th>
-    </tr>
-<?php
-    foreach ($movies as $movie) {
 
-       echo '<tr><td>'. $movie['id'] .'</td>';
-       echo '<td>'. $movie['title'] . '</td>';
-       echo '<td>'. $movie['year'] . '</td>';
-       echo '<td>'. $movie['rating'] . '</td>';
-       echo '<td><a href="details.php?movie='. $movie['slug'].'" class="btn btn-success">Voir sur le site</a>
-                  <a href="back-modif.php?id='. $movie['id'].'" class="btn btn-success">Modifier</a>
-                  <a href="back-delete.php?id='. $movie['id'].'" class="btn btn-success">Effacer</a>
-                  </td></tr>';
-     }
-  ?>
-</table>
 <?php include('inc/footerback.php'); ?>
