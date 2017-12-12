0f36e2e4b51be4b32b806de838d986f86cc87594
Guillaume Giudici <gui.giudici@gmail.com>
2017-12-12 14:22:26 +0100

    'gui'

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,7 @@ else {
 ?>
 
       <main>
-        <div id="alpha">
+        <div id="beta">
           <h1>Statistiques</h1>
           <?php
           //Nb de films dans la base de données
@@ -19,14 +19,15 @@ else {
             $query = $pdo->prepare($sql);
             $query->execute();
             $nbFilms = $query->fetchColumn(); ?>
-          <p><?php echo $nbFilms; ?> films dans la base de données.</p>
+          <p><?php echo $nbFilms; ?> films dans la base de données</p>
           <?php
           //Nb d'utilisateurs
             $sql = "SELECT COUNT(*) FROM users WHERE 1=1";
             $query = $pdo->prepare($sql);
             $query->execute();
             $users = $query->fetchColumn(); ?>
-          <p><?php echo $users; ?> utilisateurs.</p>
+          <p><?php echo $users; ?> utilisateurs</p>
+          <h2>Inscriptions par semaine :</h2>
           <?php
           //Nb d'utilisateurs inscrits par semaine depuis les 5 dernières semaines
           $nbSemaines = 5;
@@ -43,16 +44,23 @@ else {
           $userInscritsParSemaine = $totalUserInscritsDernieresSemaines/$nbSemaines; ?>
           <p><?php echo $userInscritsParSemaine; ?> utilisateurs inscrits en moyenne par semaine depuis <?php echo $nbSemaines; ?> semaines.</p><?php
           //Les 30 films les plus ajoutés à des listes de films à voir
-          $sql = "SELECT a.title
+          ?><h2>Top 30 des films les plus ajoutés à des listes de films à voir :</h2><?php
+          $sql = "SELECT a.title, COUNT(n.id_movie)
                   FROM notes AS n
                   LEFT JOIN all_movies AS a
                   ON n.id_movie = a.id
-                  WHERE n.note IS NULL";
+                  WHERE n.note IS NULL
+                  GROUP BY n.id_movie
+                  ORDER BY COUNT(n.id_movie) DESC LIMIT 30";
           $query = $pdo->prepare($sql);
           $query->execute();
           $filmsLesPlusAVoir = $query->fetchAll();
           // debug($filmsLesPlusAVoir);
-          ?>
+          ?><ul><?php
+          foreach ($filmsLesPlusAVoir as $topFilm) { ?>
+            <li class="disc"><?php echo $topFilm['title'] ?></li><?php
+          }
+          ?></ul>
         </div>
       </main>
 
